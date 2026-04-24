b9c60ecb932972fb6c96e90c5ed1559f64d09c10
kabledev <101965969+kabledev@users.noreply.github.com>
2026-04-24 14:43:46 +0300

    build: use tsup callback to detect dev mode via watch flag

--- packages/vite/tsup.config.ts
+++ packages/vite/tsup.config.ts
@@ -1,25 +1,28 @@
 import { defineConfig } from "tsup";
 
-export default defineConfig({
-  entry: {
-    index: "src/index.ts",
-  },
-  format: ["esm", "cjs"],
-  outDir: "dist",
-  platform: "node",
-  target: "node20",
-  external: ["vite"],
-  ///
-  dts: {
-    compilerOptions: {
-      ignoreDeprecations: "6.0",
+export default defineConfig((options) => {
+  const isDev = !!options.watch;
+  return {
+    entry: {
+      index: "src/index.ts",
     },
-  },
-  splitting: false,
-  shims: true,
-  clean: true,
-  //skipNodeModulesBundle: true,
-  minify: false,
-  sourcemap: false,
-  treeshake: true,
+    format: ["esm", "cjs"],
+    outDir: "dist",
+    platform: "node",
+    target: "node20",
+    external: ["vite"],
+    ///
+    dts: {
+      compilerOptions: {
+        ignoreDeprecations: "6.0",
+      },
+    },
+    splitting: false,
+    shims: true,
+    clean: true,
+    //skipNodeModulesBundle: true,
+    minify: !isDev,
+    sourcemap: isDev,
+    treeshake: true,
+  };
 });
